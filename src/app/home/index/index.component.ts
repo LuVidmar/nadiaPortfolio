04b425f0d00f7e30872ea9ce49c6d72b336a306f
Luvito <lucas.vidmar@gmail.com>
2021-01-18 20:33:39 -0300

    index

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -9,6 +9,29 @@ export class IndexComponent implements OnInit {
 
   constructor() { }
 
+  categories = [
+    {
+      title: '🎓 Academic',
+      about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      link: '/academic'
+    },
+    {
+      title: '🏅 Sports',
+      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      link: '/sport'
+    },
+    {
+      title: '🙋‍♀️ Volunteering',
+      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      link: '/volunteering'
+    },
+    {
+      title: '💆 Hobbies',
+      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      link: '/hobbies'
+    }
+  ];
+
   ngOnInit(): void {
   }
 
