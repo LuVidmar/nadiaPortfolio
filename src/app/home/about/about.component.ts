364a7e2904d7dd96cd3c4d8f77ed88e6e54bfaf3
Luvito <lucas.vidmar@gmail.com>
2021-01-22 11:12:24 -0300

    icon, and navbar fixes

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -10,6 +10,17 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.CalculateAge();
+  }
+
+  public birthdate: Date = new Date(2004,10,7);
+  public age: number;
+
+  public CalculateAge(): void {
+    if (this.birthdate) {
+      var timeDiff = Math.abs(Date.now() - new Date(this.birthdate).getTime());
+      this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    }
   }
 
 }
--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -12,22 +12,22 @@ export class IndexComponent implements OnInit {
   categories = [
     {
       title: '🎓 Academic',
-      about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      about: 'Here I will post every academic activity, competition or archievemnt along with documents, photos and backstories to back them up.',
       link: '/academic'
     },
     {
       title: '🏅 Sports',
-      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      about: 'In this section you will find the sports Ive practiced over the years and some photos I feel like sharing.',
       link: '/sports'
     },
     {
       title: '🙋‍♀️ Volunteering',
-      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      about: 'Eventhough volunteering is an habit of lately, I would like to keep a good record of what Ive done to remind myself of whats important.',
       link: '/volunteering'
     },
     {
       title: '💆 Hobbies',
-      about: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti consequuntur alias. Sit vitae perspiciatis officiis, dolorem tempore impedit explicabo nihil consequuntur minus necessitatibus voluptatibus asperiores! Ea blanditiis et veniam!',
+      about: 'The things I spend my free time on will appear in this page. I will try to update the status of each one when I make progress.',
       link: '/hobbies'
     }
   ];
